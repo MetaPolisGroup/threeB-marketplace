39004fd167e22edfff8b5d8c74b3744e7bac6ea5
chainman2209 <chainsawman2209@gmail.com>
2023-04-06 17:20:40 +0700

    feat: add nft detail

--- src/components/elements/navigation/NavBar/SideBarItem.tsx
+++ src/components/elements/navigation/NavBar/SideBarItem.tsx
@@ -3,29 +3,20 @@ import Image from 'next/image';
 import React from 'react';
 
 interface NavItemProps extends FlexProps {
-    src: string;
-    children: React.ReactNode;
+  src: string;
+  children: React.ReactNode;
+  path: string;
 }
 
-const NavItem = ({ src, children, ...rest }: NavItemProps) => {
-    return <Link href="#" style={{ textDecoration: 'none' }} _focus={{ boxShadow: 'none' }}>
-        <Flex
-            align="center"
-            borderRadius="lg"
-            role="group"
-            cursor="pointer"
-            my='20px'
-           
-            {...rest}>
-            {src && (
-                <Image src={src}
-                    width={32}
-                    height={32}
-                />
-            )}
-            {children}
-        </Flex>
+const NavItem = ({ src, children, path, ...rest }: NavItemProps) => {
+  return (
+    <Link href={path} style={{ textDecoration: 'none' }} _focus={{ boxShadow: 'none' }}>
+      <Flex align="center" borderRadius="lg" role="group" cursor="pointer" my="20px" {...rest}>
+        {src && <Image src={src} width={32} height={32} />}
+        {children}
+      </Flex>
     </Link>
-}
+  );
+};
 
-export default NavItem
+export default NavItem;
