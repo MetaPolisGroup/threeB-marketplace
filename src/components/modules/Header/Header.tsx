7a3f9297470b33c97b1846bcae0790bba9fc631c
Duy Khuong <khuongtran227@gmail.com>
2023-04-06 14:46:48 +0700

    card UI

--- src/components/modules/Header/Header.tsx
+++ src/components/modules/Header/Header.tsx
@@ -1,12 +1,12 @@
 import { Box, Flex, HStack } from '@chakra-ui/react';
-import { SearchComponent } from 'components/elements';
+import SearchComponent from 'components/elements/search/SearchComponent';
 
 import { ButtonConnect } from '../ButtonConnect';
 
 const Content = () => {
   return (
     <Box borderBottom="1px" borderBottomColor="chakra-border-color">
-      <Flex align="center" justify="space-between">
+      <Flex align="center" justify="space-between" pb='36px'>
         <SearchComponent />
         <HStack spacing={{ base: '0', md: '6' }}>
           <Flex alignItems={'center'}>
