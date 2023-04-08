ba54b54f6be56d6782b4257cf4e88ab31a925b4a
chainman2209 <chainsawman2209@gmail.com>
2023-04-08 10:01:14 +0700

    fix: profile in my collection

--- src/components/templates/balances/NFT/NFTBalances.tsx
+++ src/components/templates/balances/NFT/NFTBalances.tsx
@@ -4,27 +4,30 @@ import { useEvmWalletNFTs } from '@moralisweb3/next';
 import { useAccount, useNetwork } from 'wagmi';
 import { useEffect } from 'react';
 import { Box } from '@chakra-ui/react';
+import Profile from 'components/templates/profile';
 
 const NFTBalances = () => {
   const { chain } = useNetwork();
-  const { address } = useAccount();
+  const { address, isConnected } = useAccount();
   const { data: nfts } = useEvmWalletNFTs({
     address: address ? address : '',
     chain: chain?.id,
   });
 
-  useEffect(() => console.log('nfts: ', nfts), [nfts]);
+  useEffect(() => {
+    console.log('nfts: ', nfts);
+  }, [nfts]);
 
   return (
     <>
-      <Heading size="lg" marginBottom={6} margin={["20px 0px", ""]}>
+      {isConnected ? <Profile /> : null}
+      <Heading size="lg" marginBottom={6} margin={['20px 0px', '']}>
         NFT Balances
       </Heading>
 
       {nfts?.length ? (
         <div className="nft">
-          <Grid templateColumns="repeat(auto-fill, 350px)" rowGap={5} justifyItems='center'
-          >
+          <Grid templateColumns="repeat(auto-fill, 350px)" rowGap={5} justifyItems="center">
             {nfts.map((nft, key) => (
               <GridItem key={key}>
                 <NFTCard nft={nft} />
