959ebdd5b41b82571726381aae6b417e3fa78af1
Nguyễn Thiên Ân <an.nt.techdev@gmail.com>
2023-04-14 09:14:35 +0700

    chore: fix upsert profile

--- pages/api/user/index.ts
+++ pages/api/user/index.ts
@@ -8,20 +8,10 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       break;
     }
     case 'POST': {
-      const body = req.body;
-      if (!body.wallet_address || !body.name || !body.email || !body.phone) {
-        res.status(400).json('Wallet Address | Name | Email | Phone is undefined');
-      } else {
-        const result = await firebase.upsert('moralis', body.wallet_address, body);
-        res.status(200).json({ result });
-      }
-      break;
-    }
-    case 'POST': {
       const { data } = req.body;
       if (data) {
         if (!data.wallet_address || !data.name || !data.email || !data.phone) {
-          res.status(400).json('Missing main fields');
+          res.status(400).json('Wallet Address | Name | Email | Phone is undefined');
         } else {
           const result = await firebase.upsert('moralis', data.wallet_address, data);
           res.status(200).json({ result });
